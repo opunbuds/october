8bd3ac3f0d6f801f24b0c288ced3eb46246f8091
opunbuds <opunbuds@gmail.com>
2015-12-11 12:04:36 +0700

    Added PHPDOTNEV to set environment variable

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => 'localhost',
-            'port'      => '',
-            'database'  => 'database',
-            'username'  => 'root',
-            'password'  => '',
+            'host'      => getenv('DB_HOST'),
+            'port'      => getenv('DB_PORT'),
+            'database'  => getenv('DB_NAME'),
+            'username'  => getenv('DB_USERNAME'),
+            'password'  => getenv('DB_PASSWORD'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
